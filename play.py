1c1cc9e56a3819287fb7db29aea00f3b9e7d50be
Irene Ngoi Sze Chien <chiensze1021@gmail.com>
2024-08-22 14:43:25 +0800

    upgrade1

--- play.py
+++ play.py
@@ -108,6 +108,182 @@ dimsum_TA = pygame.transform.scale(dimsum_TA, (100,100))
 
 #option box(put food's image init)
 
+#draw text display
+def draw_text(text, font, color, surface, x, y):
+    text_obj = font.render(str(text), True, color)
+    text_rect = text_obj.get_rect()
+    text_rect.center = (x, y)
+    surface.blit(text_obj, text_rect)
+
+class MachineButton():
+    def __init__(self, x, y, image_path, scale):
+        self.image = pygame.image.load(image_path)
+        width = self.image.get_width()
+        height = self.image.get_height()
+        self.image = pygame.transform.scale(self.image, (int(width*scale), int(height*scale)))
+        self.rect = self.image.get_rect(topleft = (x, y)) #set button position
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect) #draw button on screen
+    
+    def is_clicked(self, pos):
+        return self.rect.collidepoint(pos) #check if the mouse position is inside the button's rect
+
+class YesButton():
+    def __init__(self, x, y, image_path, scale):
+        self.image = pygame.image.load(image_path)
+        width = self.image.get_width()
+        height = self.image.get_height()
+        self.image = pygame.transform.scale(self.image, (int(width*scale), int(height*scale)))
+        self.rect = self.image.get_rect(topleft = (x, y)) #set button position
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect) #draw button on screen
+    
+    def is_clicked(self, pos):
+        return self.rect.collidepoint(pos) #check if the mouse position is inside the button's rect
+
+class NoButton():
+    def __init__(self, x, y, image_path, scale):
+        self.image = pygame.image.load(image_path)
+        width = self.image.get_width()
+        height = self.image.get_height()
+        self.image = pygame.transform.scale(self.image, (int(width*scale), int(height*scale)))
+        self.rect = self.image.get_rect(topleft = (x, y)) #set button position
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect) #draw button on screen
+    
+    def is_clicked(self, pos):
+        return self.rect.collidepoint(pos) #check if the mouse position is inside the button's rect
+
+class UpgradeButton():
+    def __init__(self, x, y, image_path, scale):
+        self.image = pygame.image.load(image_path)
+        width = self.image.get_width()
+        height = self.image.get_height()
+        self.image = pygame.transform.scale(self.image, (int(width*scale), int(height*scale)))
+        self.rect = self.image.get_rect(topleft = (x, y)) #set button position
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect) #draw button on screen
+    
+    def is_clicked(self, pos):
+        return self.rect.collidepoint(pos) #check if the mouse position is inside the button's rect
+
+# Set up the main window
+screen = pygame.display.set_mode((1400, 750))
+pygame.display.set_caption("Pop-up Example")
+
+#font
+font = pygame.font.SysFont("freesansbold.ttf", 26)
+
+#rectangle to display money
+input_rect = pygame.Rect(740,125, 200, 33)
+color = pygame.Color("slategray")
+
+# Load the background image
+background = pygame.image.load("background with logo.png")
+background = pygame.transform.scale(background, (1400, 750))
+
+#load machine button images
+machineA_img = pygame.image.load("machineA.png").convert_alpha()
+lock_machineB_img = pygame.image.load("lockMachineB.png").convert_alpha()
+lock_machineC_img = pygame.image.load("lockMachineC.png").convert_alpha()
+
+# Button setup
+upgrade_btn = UpgradeButton(30, 460, "upgrade-button.png",0.25)
+default_machineA_button = MachineButton(600, 190, "machineA.png", 0.6)
+lock_machineB_button = MachineButton(780, 190, "lockMachineB.png", 0.18)
+lock_machineC_button = MachineButton(950,190, "lockMachineC.png", 0.18)
+yes_button =  YesButton(550,460, "yesButton.png", 0.4)
+no_button =  NoButton(680,463, "noButton.png", 0.38)
+
+#machine criteria
+machine_criteria = { 
+    "B": "Criteria - Cooking process speed up to 40s",
+    "C": "Criteria - Cooking process speed up to 30s ",
+}
+
+# Pop-up 1st window size (big)
+popup_width = 800
+popup_height = 600
+popup_rect = pygame.Rect(400, 90, popup_width, popup_height)
+
+# Pop-up 2nd window size (medium)
+popup2_width = 750
+popup2_height = 300
+popup2_rect = pygame.Rect(423, 380, popup2_width, popup2_height)
+
+# Pop-up 3nd window size (small)
+popup3_width = 720
+popup3_height = 160
+popup3_rect = pygame.Rect(438, 500, popup3_width, popup3_height)
+
+# Main loop
+show_popup = False
+show_popup2 = False
+show_popup3 = False
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if upgrade_btn.is_clicked(event.pos): #check if the button is clicked
+                show_popup = not show_popup
+            elif lock_machineB_button.is_clicked(event.pos):
+                show_popup2 = not show_popup2
+            elif lock_machineC_button.is_clicked(event.pos):
+                show_popup2 = not show_popup2
+            elif yes_button.is_clicked(event.pos):
+                show_popup3 = not show_popup3
+            elif no_button.is_clicked(event.pos):
+                show_popup3 = not show_popup3
+
+
+    # Draw background
+    screen.blit(background, (0, 0))
+
+    # Draw button
+    upgrade_btn.draw(screen)
+
+    # Draw popup if button clicked
+    if show_popup:
+        pygame.draw.rect(screen, (255, 204, 229), popup_rect)
+        pygame.draw.rect(screen, (148, 5, 100), popup_rect, 3)  # Popup border
+        pygame.draw.rect(screen, color, input_rect)
+
+        draw_text("Money: ", font, "black", screen, 700, 140)
+        draw_text("Machine Types: ", font, "black", screen, 510, 250)
+        draw_text("Default", font, "black", screen, 660, 315)
+        draw_text("Machine B", font, "black", screen, 840, 315)
+        draw_text("Machine C", font, "black", screen, 1010, 315)
+        draw_text("Machine Features - B: Features - Cooking process speed up to 40s", font, "red", screen, 714, 340)
+        draw_text("- C: Features - Cooking process speed up to 30s", font, "red", screen, 788, 360)
+
+        if lock_machineB_button.draw(screen):
+            print("machineB lock")
+        elif lock_machineC_button.draw(screen):
+            print("machineC lock")
+        elif default_machineA_button.draw(screen):
+            print("Default machine")
+
+    if show_popup2:
+        pygame.draw.rect(screen,(222, 201, 235), popup2_rect)
+
+        draw_text("INSTRUCTIONS: ", font, "black", screen, 504, 400)
+        draw_text("--> Your money now can only upgrade to Machine B. ", font, "black", screen, 639, 423)
+        draw_text("--> So, do you want to upgrade your current machine to machine B? ", font, "black", screen, 705, 448)
+    
+        yes_button.draw(screen)
+        no_button.draw(screen)
+
+    if show_popup3:
+        pygame.draw.rect(screen,(201, 148, 220), popup3_rect)
+
 #button class
 class Button():
     def __init__(self, x, y, image, scale):
